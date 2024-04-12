2bcd391c3c3c3be34a383050c2079d32568d51de
Andresa <andresa_15ga@hotmail.com>
2024-04-12 18:11:31 -0300

    feat: construção do banner carousel

--- src/components/banner-texts/index.jsx
+++ src/components/banner-texts/index.jsx
@@ -1,18 +1,26 @@
 import Sustentabilidade from "../../assets/sustentabilidade.png"
 import Check from "../../assets/check.png"
+import PropTypes from "prop-types";
 
-export default function BannerTextComponent() {
+
+export default function BannerTextComponent({ infoBanner1, infoBanner2, altProp }) {
     return (
         <div className="list-text-banner">
             <div>
-                <img src={Sustentabilidade} alt="simbolo sustentabilidade" width={55} height={55} />
-                <p>Promovemos o ciclo de destinação com responsabilidade ambiental.</p>
+                <img src={Sustentabilidade} alt={altProp} width={55} height={55} />
+                <p>{infoBanner1}</p>
             </div>
 
             <div>
-            <img src={Check} alt="simbolo sustentabilidade" width={55} height={55} />
-                <p>Executamos o serviço de acordo com o Ibama e a Cetesb.</p>
+                <img src={Check} alt={altProp} width={55} height={55} />
+                <p>{infoBanner2}</p>
             </div>
         </div>
     )
 }
+
+BannerTextComponent.propTypes = {
+    infoBanner1: PropTypes.string.isRequired,
+    infoBanner2: PropTypes.string.isRequired,
+    altProp: PropTypes.string.isRequired,
+};
